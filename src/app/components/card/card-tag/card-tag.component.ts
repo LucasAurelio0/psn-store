26b271f56b9921c0d9e811d15f7174870b01cb6c
LucasAurelio0 <zeaureliosv@gmail.com>
2024-11-04 11:09:20 -0300

    feat: add card component

---
+++ src/app/components/card/card-tag/card-tag.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-tag',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-tag.component.html',
+  styleUrl: './card-tag.component.css'
+})
+export class CardTagComponent {
+
+}

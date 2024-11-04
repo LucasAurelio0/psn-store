fdd3efef54cfde5b8ce4b526956e11aaa690540b
LucasAurelio0 <zeaureliosv@gmail.com>
2024-11-04 10:51:28 -0300

    feat: header component + assets folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { CardComponent } from "./components/card/card.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent, CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

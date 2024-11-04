9edf965fc26cf585d2d596f8e244c099e404f9a9
LucasAurelio0 <zeaureliosv@gmail.com>
2024-11-04 11:26:22 -0300

    feat: implement card tag component + input binding

--- src/app/components/card/card-tag/card-tag.component.ts
+++ src/app/components/card/card-tag/card-tag.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-tag',
@@ -8,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './card-tag.component.css'
 })
 export class CardTagComponent {
+
+	@Input()
+	cardTag:string = ''
 
 }
--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CardTagComponent } from "./card-tag/card-tag.component";
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [CardTagComponent],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
